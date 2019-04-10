76177adffb52dcc3568ab48ec74f4bf622c293f9
cary <carynux@gmail.com>
2019-04-10 23:45:32 +0800

    项目结构，组件样式完善

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <router-view></router-view>
+    <Header id="header"></Header>
+    <main id="main">
+      <router-view></router-view>
+    </main>
+    <Footer id="footer"></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
-<style>
+<style lang="less">
+@import "./assets/common.less";
+
+html,body,#app{
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  grid-template-columns: 12% auto 12%;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas: "header header header"
+                       ".      main ."
+                       "footer footer footer";
+  // .代表不占区块
+  #header{
+    grid-area:header;
+    padding-left: 12%;
+    padding-right: 12%;
+  }
+  #main{
+    grid-area: main;
+  }
+  #footer{
+    grid-area: footer;
+    padding-left: 12%;
+    padding-right: 12%;
+  }
+}
+
+@media (max-width:768px) {
+  #app{
+    grid-template-columns: 10px auto 10px;
+    #header,#footer{
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+  }
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>© cary.ren 2018 wechat: xiaoxiaoafei001</p>
+  </footer>
+</template>
+<style>
+  footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
