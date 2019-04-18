b6ae00536cbc97845f69c9a2c8b94692faf3a07f
cary <carynux@gmail.com>
2019-04-18 23:31:49 +0800

    完善vuex的登录、注册、退出

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,16 +1,43 @@
+import auth from '@/api/auth'
 const state = {
-
+    user:null,
+    isLogin:false
 }
 const getters = {
 
 }
 
 const mutations = {
-
+    setUser(state,payload){
+        state.user = payload.user
+    },
+    setLogin(state,payload){
+        state.isLogin = payload.isLogin
+    }
 }
 
 const actions = {
-
+    login({ commit },{username,password}){
+        return auth.login({username,password})
+            .then(res =>{
+                commit('setUser', {user: res.data})
+                commit('setLogin', {isLogin: true})
+            })
+    },
+    async register({ commit },{username,password}){
+        let res = await auth.register({username,password})
+        commit('setUser', {user: res.data})
+        commit('setLogin', {isLogin: true})
+        return res.data
+    },
+    async logout({commit,state}){
+        if(state.isLogin) return true
+        let res = await auth.getInfo()
+        commit('setLogin',{isLogin:res.isLogin})
+        if(!res.isLogin) return false
+        commit('setUser',{user:res.data})
+        return true
+    }
 }
 export default{
     state,
